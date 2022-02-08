4f0426b209d9047bf2ad976a15f4323c34c00f64
SaydAliUsmanov <usmanov.guno@mail.ru>
2022-02-08 12:03:34 +0300

    Создал базовые компоненты

---
+++ src/stories/Cell/Cell.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Cell } from '../../components/Cell/Cell';
+
+export default {
+  title: 'Cell',
+  component: Cell,
+};
+
+const Template = () => {
+  return <Cell value="" />;
+};
+export const Default = Template;
