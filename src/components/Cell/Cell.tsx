4033dae2db1d2c0412472e2801dd075b0f549c5a
SaydAliUsmanov <usmanov.guno@mail.ru>
2022-02-08 17:50:14 +0300

    Добавил ключ color в пропс компанента Cell

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 type CellProps = {
   value: string;
+  color: string;
 };
 
-export const Cell = ({ value }: CellProps) => {
+export const Cell = ({ value, color }: CellProps) => {
+  const classNameColor = `w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded ${color}`;
+
   return (
-    <div className="w-14 h-14 border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-black">
+    <div className={classNameColor}>
       <div>{value}</div>
     </div>
   );
