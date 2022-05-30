bd32a53cd9505bf2abbd483e71c17bf0ff216f83
kuduzow <admin@intocode.ru>
2022-05-30 23:08:48 +0300

    Добавлена логика появления модального окна по истечении попыток

--- src/components/GameModals/GameModals.tsx
+++ src/components/GameModals/GameModals.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import GameOverModal from './GameOverModal';
 import WordGuessedModal from './WordGuessedModal';
 
 const GameModals = () => {
-  return <WordGuessedModal />;
+  return (
+    <>
+      <GameOverModal />
+      <WordGuessedModal />
+    </>
+  );
 };
 
 export default GameModals;
---
+++ src/components/GameModals/GameOverModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BG_GREEN } from '../../constants';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import Modal from '../Modal/Modal';
+import ModalTitle from '../Modal/ModalTitle';
+
+const GameOverModal = () => {
+  const dispatch = useAppDispatch();
+
+  const gameOver = useAppSelector((state) => state.gameOver);
+  const record = useAppSelector((state) => state.record);
+
+  const handleStartNewGame = () => {
+    // todo: сделать санк для старта новой игры
+    dispatch({ type: 'noop' }); // просто заглушка
+  };
+
+  return (
+    <Modal opened={gameOver} onClose={handleStartNewGame}>
+      <ModalTitle>Игра окончена</ModalTitle>
+
+      <div>
+        <div>У тебя не осталось больше попыток.</div>
+        <div>
+          Твой рекорд: <strong>{record}</strong>
+        </div>
+        <div>Можно начать новую игру с нуля.</div>
+        <div className="flex justify-center mt-6">
+          <button
+            onClick={handleStartNewGame}
+            type="button"
+            className={`${BG_GREEN} p-3 text-white rounded-sm`}
+          >
+            Начать заново
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default GameOverModal;
