c668786e5eff385f485e3c4f90faa843790ef3a8
Kuduzow Akhmad <ammya@ya.ru>
2022-06-04 12:38:16 +0300

    Изменен адрес сервера и логика его изменения (#24)
    Co-authored-by: kuduzow <admin@intocode.ru>

--- src/constants.ts
+++ src/constants.ts
@@ -7,3 +7,6 @@ export const BG_GRAY = 'bg-neutral-500';
 
 // количество миллисекунд, после которого закрывается info-сообщение
 export const INFO_DISAPPEAR_MS = 2000;
+
+export const SERVER_URL =
+  process.env.REACT_APP_SERVER_URL || 'https://wordle-server.intocode.ru';
